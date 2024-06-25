cb6058801a7883312ee8ee62c3aa64a8bc75417a
rabindra789 <rabindrameher116@gmail.com>
2024-06-25 10:03:32 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
@@ -14,5 +15,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// Routes import
+import userRouter from "./routes/user.route.js"
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
 
 export { app };
